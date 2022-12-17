eaef4ad4826b864f0ebe233b75a35064e341a044
dezy-x2 <df.dezmond9@gmail.com>
2022-12-17 16:20:46 -0500

    linkedList header created

---
+++ linkedList.h
@@ -0,0 +1,18 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "node.h"
+using namespace std;
+
+class LinkedList {
+    private:
+        int size;
+        Node* headNode;
+    public:
+        LinkedList();
+        LinkedList(int size, Node* headNode);
+        void add(Node* newNode);
+        Node* get(int at);
+        Node* pop();
+};
+
+#endif
